c2fe2bbefd5bd51c6b894311252300dad912ff60
purple_elephant <46032236+purpleElephantWithKikimerOnItsBack@users.noreply.github.com>
2019-05-04 21:36:43 +0300

    Add files via upload

---
+++ scripts/timer.js
@@ -0,0 +1,35 @@
+// Set the date we're counting down to
+let countDownDate = new Date();
+countDownDate.setHours(24, 0, 0, 0);
+
+// Update the count down every 1 second
+const x = setInterval(function() {
+
+  // Get todays date and time
+
+  const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+
+  const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
